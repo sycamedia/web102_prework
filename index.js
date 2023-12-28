0f1d34a569931e02b4806cef20541d78e5a172dd
sycamedia <parkerdennis413@gmail.com>
2023-12-28 10:06:53 -0500

    Fix issue with curly brace placement

--- index.js
+++ index.js
@@ -28,13 +28,10 @@ const gamesContainer = document.getElementById("games-container");
 // create a function that adds all data from the games array to the page
 function addGamesToPage(games) {
 
-    console.log("function is about to loop")
-
     // loop over each item in the data
     for (var game of games) {
-    }
         // create a new div element, which will become the game card
-        let newDiv = document.createElement("div");
+        var newDiv = document.createElement("div");
 
         // add the class game-card to the list
         newDiv.classList.add("game-card");
@@ -44,19 +41,21 @@ function addGamesToPage(games) {
         // TIP: if your images are not displaying, make sure there is space
         // between the end of the src attribute and the end of the tag ("/>")
         newDiv.innerHTML = `
+        <h1>${game.name}</h1>
         <img src=${game.img} />
         <p>${game.description}</p>
         <p>Pledged: ${game.pledged} of ${game.goal} goal`;
 
         // append the game to the games-container
-        gamesContainer.appendChild(newDiv);
+        gamesContainer.append(newDiv);
+    }
 }
 
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
 
 addGamesToPage(GAMES_JSON);
-console.log("called the function");
+
 
 /*************************************************************************************
  * Challenge 4: Create the summary statistics at the top of the page displaying the
